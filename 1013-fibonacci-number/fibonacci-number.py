bccc7e772e61e7fd6f87bed113af3baecfca9034
sriram99895 <100017712+sriram99895@users.noreply.github.com>
2024-02-09 17:26:24 +0530

    Time: 26 ms (97.84%) | Memory: 16.5 MB (81.72%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,11 +1,15 @@
 class Solution:
-    def fibi(self,n):
+    def fibi(self,n,dp):
         if n <= 1:
-            return n    
-        a = self.fibi(n-1)
-        b = self.fibi(n-2)
-        return a+b    
+            return n  
+        if dp[n]!=-1:
+            return dp[n]      
+        a = self.fibi(n-1,dp)
+        b = self.fibi(n-2,dp)
+        dp[n] = a+b
+        return dp[n]    
     def fib(self, n: int) -> int:
-        k = self.fibi(n)
+        dp = [-1 for i in range(n+1)]
+        k = self.fibi(n,dp)
         return k
         
